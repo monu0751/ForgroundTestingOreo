3bf5cb82bddd377238d5c5dd29239bb217f53ff7
abhi0751 <abhinavsengar.cs@gmail.com>
2019-02-26 13:59:11 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mac/forgroundtestingoreo/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.mac.forgroundtestingoreo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+
+
+public class MainActivity extends AppCompatActivity{ // implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setTitle("dev2qa.com - Android Foreground Service Example.");
+
+        Button startServiceButton = (Button)findViewById(R.id.start_foreground_service_button);
+        startServiceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MyForeGroundService.class);
+                intent.setAction(MyForeGroundService.ACTION_START_FOREGROUND_SERVICE);
+                startService(intent);
+            }
+        });
+
+        Button stopServiceButton = (Button)findViewById(R.id.stop_foreground_service_button);
+        stopServiceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MyForeGroundService.class);
+                intent.setAction(MyForeGroundService.ACTION_STOP_FOREGROUND_SERVICE);
+                startService(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/mac/forgroundtestingoreo/MyForeGroundService.java
@@ -0,0 +1,155 @@
+package com.example.mac.forgroundtestingoreo;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+import android.support.v4.app.NotificationCompat;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.graphics.Color;
+import android.content.Context;
+import android.os.Build;
+
+
+public class MyForeGroundService extends Service {
+
+    private static final String TAG_FOREGROUND_SERVICE = "FOREGROUND_SERVICE";
+
+    public static final String ACTION_START_FOREGROUND_SERVICE = "ACTION_START_FOREGROUND_SERVICE";
+
+    public static final String ACTION_STOP_FOREGROUND_SERVICE = "ACTION_STOP_FOREGROUND_SERVICE";
+
+    public static final String ACTION_PAUSE = "ACTION_PAUSE";
+
+    public static final String ACTION_PLAY = "ACTION_PLAY";
+
+    public MyForeGroundService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG_FOREGROUND_SERVICE, "My foreground service onCreate().");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (intent != null) {
+            String action = intent.getAction();
+
+            switch (action) {
+                case ACTION_START_FOREGROUND_SERVICE:
+                    startForegroundService();
+                    Toast.makeText(getApplicationContext(), "Foreground service is started.", Toast.LENGTH_LONG).show();
+                    break;
+                case ACTION_STOP_FOREGROUND_SERVICE:
+                    stopForegroundService();
+                    Toast.makeText(getApplicationContext(), "Foreground service is stopped.", Toast.LENGTH_LONG).show();
+                    break;
+                case ACTION_PLAY:
+                    Toast.makeText(getApplicationContext(), "You click Play button.", Toast.LENGTH_LONG).show();
+                    break;
+                case ACTION_PAUSE:
+                    Toast.makeText(getApplicationContext(), "You click Pause button.", Toast.LENGTH_LONG).show();
+                    break;
+            }
+        }
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    /* Used to build and start foreground service. */
+    private void startForegroundService() {
+        Log.d(TAG_FOREGROUND_SERVICE, "Start foreground service.");
+
+        // Create notification default intent.
+        Intent intent = new Intent();
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        // Create notification builder.
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+
+        // Make notification show big text.
+        NotificationCompat.BigTextStyle bigTextStyle = new NotificationCompat.BigTextStyle();
+        bigTextStyle.setBigContentTitle("Music player implemented by foreground service.");
+        bigTextStyle.bigText("Android foreground service is a android service which can run in foreground always, it can be controlled by user via notification.");
+        // Set big text style.
+        builder.setStyle(bigTextStyle);
+
+        builder.setWhen(System.currentTimeMillis());
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        Bitmap largeIconBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher_background);
+        builder.setLargeIcon(largeIconBitmap);
+        // Make the notification max priority.
+        builder.setPriority(Notification.PRIORITY_MAX);
+        // Make head-up notification.
+        builder.setFullScreenIntent(pendingIntent, true);
+
+        // Add Play button intent in notification.
+        Intent playIntent = new Intent(this, MyForeGroundService.class);
+        playIntent.setAction(ACTION_PLAY);
+        PendingIntent pendingPlayIntent = PendingIntent.getService(this, 0, playIntent, 0);
+        NotificationCompat.Action playAction = new NotificationCompat.Action(android.R.drawable.ic_media_play, "Play", pendingPlayIntent);
+        builder.addAction(playAction);
+
+        // Add Pause button intent in notification.
+        Intent pauseIntent = new Intent(this, MyForeGroundService.class);
+        pauseIntent.setAction(ACTION_PAUSE);
+        PendingIntent pendingPrevIntent = PendingIntent.getService(this, 0, pauseIntent, 0);
+        NotificationCompat.Action prevAction = new NotificationCompat.Action(android.R.drawable.ic_media_pause, "Pause", pendingPrevIntent);
+        builder.addAction(prevAction);
+
+        // Build the notification.
+        Notification notification = builder.build();
+
+        // Start foreground service.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+            startMyOwnForeground();
+        else
+            startForeground(1, notification);
+
+
+
+    }
+
+    private void startMyOwnForeground(){
+        String NOTIFICATION_CHANNEL_ID = "com.example.mac.forgroundtestingoreo";
+        String channelName = "My Background Service";
+        NotificationChannel chan = new NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_NONE);
+        chan.setLightColor(Color.BLUE);
+        chan.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        assert manager != null;
+        manager.createNotificationChannel(chan);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+        Notification notification = notificationBuilder.setOngoing(true)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("App is running in background")
+                .setPriority(NotificationManager.IMPORTANCE_MIN)
+                .setCategory(Notification.CATEGORY_SERVICE)
+                .build();
+        startForeground(2, notification);
+    }
+
+    private void stopForegroundService() {
+        Log.d(TAG_FOREGROUND_SERVICE, "Stop foreground service.");
+
+        // Stop foreground service and remove the notification.
+        stopForeground(true);
+
+        // Stop the foreground service.
+        stopSelf();
+    }
+}
